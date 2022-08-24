b281b4ae18e3893f550cedb0b9fe6149251554d3
dathdka <dathdka@gmail.com>
2022-08-24 20:54:23 +0700

    edit format

--- solution_1.js
+++ solution_1.js
@@ -1,29 +1,10 @@
-
-// Cach 1: 
-var rotate = function(nums, k) {
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    var temp = [];
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    for(let i = 0; i<nums.length ;i++){
-        // Momo: 0327.227.902
-        //Vietcombank: 0201000705025
-        temp[(i+k)%nums.length] = nums[i];
-        // Momo: 0327.227.902
-        //Vietcombank: 0201000705025
-    }
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    for(let i =0 ; i<nums.length; i++){
-        // Momo: 0327.227.902
-        //Vietcombank: 0201000705025
-        nums[i] = temp[i];
-        // Momo: 0327.227.902
-        //Vietcombank: 0201000705025
-    }
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
+// Cach 1:
+var rotate = function (nums, k) {
+  var temp = [];
+  for (let i = 0; i < nums.length; i++) {
+    temp[(i + k) % nums.length] = nums[i];
+  }
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = temp[i];
+  }
 };
-// Momo: 0327.227.902
-// Hoang Dat
--- solution_2.js
+++ solution_2.js
@@ -1,46 +1,15 @@
-var rotate = function(nums, k) {
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    var temp = [];
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    var loop = k%nums.length;
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    var j = 0;
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    for(let i = nums.length - loop ; i<nums.length ;i++){
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-        temp[j] = nums[i];
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-        j++;
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    }
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    for(let i =nums.length - 1 ; i > loop - 1; i--){
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-        nums[i] = nums[i-loop];
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    }
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    j = 0;
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    for(let i = 0 ; i < loop ; i++){
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-        nums[i] = temp[i];  
-        // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
-    }
-    // Momo: 0327.227.902
-    //Vietcombank: 0201000705025
+var rotate = function (nums, k) {
+  var temp = [];
+  var loop = k % nums.length;
+  var j = 0;
+  for (let i = nums.length - loop; i < nums.length; i++) {
+    temp[j] = nums[i];
+    j++;
+  }
+  for (let i = nums.length - 1; i > loop - 1; i--) {
+    nums[i] = nums[i - loop];
+  }
+  for (let i = 0; i < loop; i++) {
+    nums[i] = temp[i];
+  }
 };
